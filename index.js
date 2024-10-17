03ac16b101ea8e1ce461ca834e2e4f41228e3f60
annagoetz1 <annalgoetz1@gmail.com>
2024-10-17 12:33:30 -0400

    Rewrite fetching logic with async/await

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const button = document.getElementById('getWeatherButton');
 
 async function getWeather () {
+  try{
     const response =  await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pittsburgh?unitGroup=metric&key=6MRGX7QE8HMREHVGNPMQWUV6L&contentType=json' , {
-   mode: 'cors'})
- .then(function(response) {
-  return response.json();
- }
- .then(function(data) {
-  console.log(data);
- })
-  .catch(function(error){
-  console.error('Error fetching data', error);
-}))}
+   mode: 'cors'});
+       // Check if the response is OK (status 200-299)
+       if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+
+
+const data = await response.json();
+
+console.log(data);
+
+    }
+    catch (error) {
+      console.error('Error fetching data', error);
+    }}
+
 button.addEventListener('click', getWeather);
