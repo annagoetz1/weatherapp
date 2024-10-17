86300fb4b7bce6288b495724f8e43d1c7399c8e7
annagoetz1 <annalgoetz1@gmail.com>
2024-10-17 12:27:23 -0400

    Add json parsing, event listener, console logging

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
+const button = document.getElementById('getWeatherButton');
+
 async function getWeather () {
-    const weatherURL =  await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pittsburgh?unitGroup=metric&key=6MRGX7QE8HMREHVGNPMQWUV6L&contentType=json' , {
-   mode: 'cors'
- });
- 
-   }
+    const response =  await fetch ('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/Pittsburgh?unitGroup=metric&key=6MRGX7QE8HMREHVGNPMQWUV6L&contentType=json' , {
+   mode: 'cors'})
+ .then(function(response) {
+  return response.json();
+ }
+ .then(function(data) {
+  console.log(data);
+ })
+  .catch(function(error){
+  console.error('Error fetching data', error);
+}))}
+button.addEventListener('click', getWeather);
